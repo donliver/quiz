2739e04f97736ede5e2ea39dcaa56992dff3033e
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-12 10:53:26 +0700

    Update  css, js at index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var x = document.getElementById("form_sample");
+var createform = document.createElement('form'); // Create New Element Form
+x.appendChild(createform);
+
 function calculateYear(days) {
   var rawYear = days / 365
   return parseInt(rawYear)
